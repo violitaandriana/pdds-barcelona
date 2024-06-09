45fbc8eba98239a9dda38dcb1c439817251777a5
Nicholas Nathanael Suhendar <104911087+Konicho46@users.noreply.github.com>
2024-06-09 14:36:16 +0700

    update

--- home.php
+++ home.php
@@ -51,7 +51,7 @@
     <div class="dropdown">
       <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
+        <strong>Admin</strong>
       </a>
       <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
         <li><a class="dropdown-item" href="#">New project...</a></li>
